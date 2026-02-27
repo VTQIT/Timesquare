d57817b88da9195b4c2f95094993c7202f6b2a77
VTQIT <lycelclanaja@gmail.com>
2026-02-27 19:58:47 +0800

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,14 +1,12 @@
+// pages/api/news.js
 export default async function handler(req, res) {
   const API_KEY = process.env.NEWS_API_KEY;
-
-  const url = `https://newsapi.org/v2/everything?q=philippines OR cebu OR world&sortBy=publishedAt&pageSize=20&apiKey=${API_KEY}`;
-
+  const url = `https://newsapi.org/v2/top-headlines?country=cn&apiKey=${API_KEY}&pageSize=20`;
   try {
     const response = await fetch(url);
     const data = await response.json();
-
     res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch news" });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch news' });
   }
 }
