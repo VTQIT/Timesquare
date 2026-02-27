e19a290223ca023f8f4a1266fee63baa1d7489d9
VTQIT <lycelclanaja@gmail.com>
2026-02-27 14:28:27 +0300

    Add files via upload

---
+++ api/news.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const API_KEY = process.env.NEWSAPI_KEY;
+
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=ph&apiKey=${API_KEY}`
+    );
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}
